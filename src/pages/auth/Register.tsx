723ee1df58f2e1ca72398d8be2674f77164fbeb9
j0nes <dreamwasp@proton.mail>
2025-02-28 09:21:21 -0500

    test commit

--- src/pages/auth/Register.tsx
+++ src/pages/auth/Register.tsx
@@ -1,5 +1,7 @@
 import { Box } from "@codecademy/gamut";
 
 export const Register: React.FC = () => {
-  return <Box>We're not accepting new members - watch this space!</Box>;
+  return (
+    <Box>We're not accepting new members - watch this space! See ya soon!</Box>
+  );
 };
